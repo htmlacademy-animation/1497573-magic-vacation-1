35ac22b4e76acda927a27df33b7e9341eed993d6
Alena Katkova <katkova.web@gmail.com>
2020-05-24 01:09:20 +0200

    change social toggler styles on click

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,22 +1,31 @@
 export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
-  let socialBlockItems = socialBlock.querySelectorAll(`.social-block__item`);
+  let icons = socialBlock.querySelectorAll(`.js-social-menu-item`);
+  let toggler = socialBlock.querySelector(`.js-social-toggler`);
 
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
-    for (let i = 0; i < socialBlockItems.length; i++) {
+    for (let i = 0; i < icons.length; i++) {
       setTimeout(() => {
-        socialBlockItems[i].classList.add(`social-block__item--show`);
+        icons[i].classList.add(`social-block__item--show`);
       }, 200 * i);
     }
   });
 
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
-    for (let i = 0; i < socialBlockItems.length; i++) {
+    for (let i = 0; i < icons.length; i++) {
       setTimeout(() => {
-        socialBlockItems[i].classList.remove(`social-block__item--show`);
+        icons[i].classList.remove(`social-block__item--show`);
       }, 200 * i);
+    }
+  });
+
+  toggler.addEventListener(`click`, function () {
+    if (!toggler.classList.contains(`social-block__toggler--open`)) {
+      toggler.classList.add(`social-block__toggler--open`);
+    } else {
+      toggler.classList.remove(`social-block__toggler--open`);
     }
   });
 };
