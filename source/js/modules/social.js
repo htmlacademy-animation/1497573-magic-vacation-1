72e8841617b6fc5b15869951d16c560582344f89
Alena Katkova <katkova.web@gmail.com>
2020-05-23 22:32:11 +0200

    social networks icons appear one by one

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,9 +1,22 @@
 export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
+  let socialBlockItems = socialBlock.querySelectorAll(`.social-block__item`);
+
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
+    for (let i = 0; i < socialBlockItems.length; i++) {
+      setTimeout(() => {
+        socialBlockItems[i].classList.add(`social-block__item--show`);
+      }, 200 * i);
+    }
   });
+
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
+    for (let i = 0; i < socialBlockItems.length; i++) {
+      setTimeout(() => {
+        socialBlockItems[i].classList.remove(`social-block__item--show`);
+      }, 200 * i);
+    }
   });
 };
